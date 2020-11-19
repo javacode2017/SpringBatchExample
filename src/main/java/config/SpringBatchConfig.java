1e0ed3eb964eaf15b7c943d832d80a9ddcc3e5c0
javacode2017 <bhujatha.edu@gmail.com>
2020-11-19 12:36:29 +0530

    step create using StepBuilderFactory ,chunk ,ItemReader ,ItemProcessor and ItemWriter

--- src/main/java/config/SpringBatchConfig.java
+++ src/main/java/config/SpringBatchConfig.java
@@ -3,17 +3,32 @@ package config;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import model.User;
 
 @Configuration
 public class SpringBatchConfig {
 
 	@Bean
-	public Job job(JobBuilderFactory jobBiBuilderFactory) {
+	public Job job(JobBuilderFactory jobBiBuilderFactory,
+							StepBuilderFactory stepBuilderFactory,
+							ItemReader<User> itemReader,
+							ItemProcessor<User, User> itemProcessor,
+							ItemWriter<User> itemWriter) {
 		
-		Step step = null;
+		Step step = stepBuilderFactory.get("ETL-File-Load")
+									.<User,User>chunk(100)
+									.reader(itemReader)
+									.processor(itemProcessor)
+									.writer(itemWriter)
+									.build();
 		Job job = jobBiBuilderFactory.get("ETL-Load")
 							.incrementer(new RunIdIncrementer())
 							.start(step)
